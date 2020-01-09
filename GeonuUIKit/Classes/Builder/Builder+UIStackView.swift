af9bd7ccc76d283b8a6de2c4678fc1ce0c22ac79
Geonu Jeon <geonu1109@daou.co.kr>
2020-01-09 13:44:32 +0900

    add stackview builder

---
+++ GeonuUIKit/Classes/Builder/Builder+UIStackView.swift
@@ -0,0 +1,40 @@
+//
+//  Builder+UIStackView.swift
+//  GeonuUIKit
+//
+//  Created by 전건우 on 2020/01/09.
+//
+
+import UIKit
+
+extension Builder where Product: UIStackView {
+    public func addArrangedSubview(_ view: UIView) -> Self {
+        self.product.addArrangedSubview(view)
+        return self
+    }
+    
+    public func addArrangedSubviews(_ views: [UIView]) -> Self {
+        views.forEach(self.product.addArrangedSubview)
+        return self
+    }
+    
+    public func axis(_ axis: NSLayoutConstraint.Axis) -> Self {
+        self.product.axis = axis
+        return self
+    }
+    
+    public func spacing(_ spacing: CGFloat) -> Self {
+        self.product.spacing = spacing
+        return self
+    }
+    
+    public func alignment(_ alignment: UIStackView.Alignment) -> Self {
+        self.product.alignment = alignment
+        return self
+    }
+    
+    public func distribution(_ distribution: UIStackView.Distribution) -> Self {
+        self.product.distribution = distribution
+        return self
+    }
+}
