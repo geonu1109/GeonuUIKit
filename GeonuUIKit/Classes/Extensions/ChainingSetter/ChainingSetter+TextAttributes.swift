d207e5bc5c00304b69abf14436996202988fd2ac
Geonu Jeon <geonu1109@daou.co.kr>
2020-01-13 17:01:36 +0900

    add chainingSetter of TextAttributes

---
+++ GeonuUIKit/Classes/Extensions/ChainingSetter/ChainingSetter+TextAttributes.swift
@@ -0,0 +1,48 @@
+//
+//  ChainingSetter+TextAttributes.swift
+//  GeonuUIKit
+//
+//  Created by 전건우 on 2020/01/13.
+//
+
+import UIKit
+
+extension NSAttributedString.TextAttributes: ChaningSettable {}
+
+extension ChainingSetter where Component: NSAttributedString.TextAttributes {
+    @discardableResult
+    public func setFont(_ font: UIFont) -> Self {
+        self.component.font = font
+        return self
+    }
+    
+    @discardableResult
+    public func setForegroundColor(_ foregroundColor: UIColor) -> Self {
+        self.component.foregroundColor = foregroundColor
+        return self
+    }
+    
+    @discardableResult
+    public func setBackgroundColor(_ backgroundColor: UIColor) -> Self {
+        self.component.backgroundColor = backgroundColor
+        return self
+    }
+    
+    @discardableResult
+    public func setKern(_ kern: NSNumber) -> Self {
+        self.component.kern = kern
+        return self
+    }
+    
+    @discardableResult
+    public func setUnderlineStyle(_ underlineStyle: NSUnderlineStyle) -> Self {
+        self.component.underlineStyle = underlineStyle
+        return self
+    }
+    
+    @discardableResult
+    public func setUnderlineColor(_ underlineColor: UIColor) -> Self {
+        self.component.underlineColor = underlineColor
+        return self
+    }
+}
